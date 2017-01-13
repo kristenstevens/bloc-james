5d40ee09e4fdd00d09e361eee6b02daf8927aaa8
Kristen Stevens <kristengstevens89@gmail.com>
2017-01-12 21:35:06 -0600

    assignment-22-bloc-jams-attempt-1

--- scripts/landing.js
+++ scripts/landing.js
@@ -9,6 +9,6 @@ var animatePoints = function(){
          points[i].style.msTransform = "scaleX(1) translateY(0)";
          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
      } 
-        revealPoint(i);
+        revealPoint(i);   
     }
 };

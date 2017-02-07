1c93121062a4337931f2c901d7af964d5bf38324
Kristen Stevens <kristengstevens89@gmail.com>
2017-02-06 22:35:40 -0600

    checkpoint-29-bloc-jams-completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,32 @@
-var pointsArray = document.getElementsByClassName('point');
  
-var revealPoint = function(points) {
-         points.style.opacity = 1;
-         points.style.transform = "scaleX(1) translateY(0)";
-         points.style.msTransform = "scaleX(1) translateY(0)";
-         points.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-     
-var animatePoints = function(points){
-    forEach(points, revealPoint);
-};
-
-
-window.onload = function() {
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-    
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
-         }
-    });
+ var animatePoints = function() {
+     var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     };
+          // #6
+     $.each($('.point'), revealPoint);
  };
+
+  $(window).load(function() {
+ 
+     // #1
+     if ($(window).height() > 950) {
+         animatePoints();
+     }
+
+     // #2
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+     // #3
+     $(window).scroll(function(event) {
+         
+         // #4
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+         }
+     });
+ });
